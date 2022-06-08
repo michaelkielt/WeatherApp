19797a571f71bcd11d1e4a7ceb370d284a48440d
michaelkielt <89637624+michaelkielt@users.noreply.github.com>
2022-06-08 20:40:52 +0100

    Add files via upload

---
+++ weatherapp/script.js
@@ -0,0 +1,44 @@
+let weather = {
+    apikey: "e99004e0f937e5e6d486c639d0b8464a",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apikey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" +icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =  "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/2560x1440/?" + name + "')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function() {
+        weather.search();
+    })
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})    
+
+weather.fetchWeather("Belfast");
